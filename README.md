eb464344c1d90bfad493c86e73665960fe2e3afa
myucesan <my96@live.nl>
2021-03-19 15:09:17 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-data-binding-my
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-data-binding-my)
---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  // Add data elements for your name, the title, year released, rating, and imdb link of your favorite movie
+  // favFlop = {
+  //   title: ,
+  //   yearReleased: ,
+  //   rating: ,
+  //   imdbLink: ,
+  // }
+}
